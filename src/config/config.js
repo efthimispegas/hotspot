e12d1771fab1178e7dd385694857e531b52eb708
Efthimis Pegas <efthimispegas@gmail.com>
2019-02-20 20:48:30 +0200

    Moved personal keys to another file

--- src/config/config.js
+++ src/config/config.js
@@ -6,7 +6,16 @@ const config = {
   RADIUS_RATIO: 0.000783932,
   UPLOAD_FOLDER: '/gallery',
   PUBLIC_FOLDER: '/public',
-  DOMAIN_URL: 'http://192.168.10.39:3000'
+  DOMAIN_URL: 'http://192.168.10.39:3000',
+  google: {
+    CLIENT_ID:
+      '924883342846-v1g24lb4r7jgboq1l3km0olofg4gbth3.apps.googleusercontent.com',
+    CLIENT_SECRET: 'a0zBGTH1loO38TdbM6OS3Tsu'
+  },
+  facebook: {
+    CLIENT_ID: '2691571627520475',
+    CLIENT_SECRET: 'ae12d6b74495de2992df48b497c77a6d'
+  }
 };
 
 export default config;
